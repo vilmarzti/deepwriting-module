e4719ca1590dd97cd7dbb444baf8fa1b315b8e0d
Martin Villavicencio <villavicencio.martin@protonmail.com>
2019-11-02 19:36:22 +0100

    included mousedraw

--- touch-writing/touch.js
+++ touch-writing/touch.js
@@ -2,6 +2,7 @@ let canvas;
 let ctx;
 let offset_x;
 let offset_y;
+let ismousedown = false
 let touchstart_pos;
 let touchmove_pos;
 
@@ -15,45 +16,108 @@ window.onload = () => {
     canvas.addEventListener('touchstart', touchstart);
     canvas.addEventListener('touchmove', touchmove);
     canvas.addEventListener('touchend', touchend);
+    canvas.addEventListener('mousedown', mousedown);
+    canvas.addEventListener('mousemove', mousemove);
+    canvas.addEventListener('mouseup', mouseup)
 }
 
 
+function mousedown(event) {
+    ismousedown = true;
+    let new_event = mouseToTouch(event);
+    touchstart(new_event)
+}
 
-function touchstart(event){
+function mousemove(event) {
+    if (ismousedown) {
+        let new_event = mouseToTouch(event);
+        touchmove(new_event)
+    }
+}
+
+function mouseup(event) {
+    let new_event = mouseToTouch(event);
+    ismousedown = false;
+    touchend(new_event);
+}
+
+function touchstart(event) {
     touchstart_pos = getRelativePosition(getTouches(event));
-    touchhistory.push({'type': 'touchstart', 'x': touchmove_pos[0], 'y': touchmove_pos[1]})
+    touchhistory.push(
+        touch_event('start', touchstart_pos[0], touchstart_pos[1], 0)
+    )
 }
 
-function touchmove(event){
+function touchmove(event) {
     touchmove_pos = getRelativePosition(getTouches(event));
     ctx.beginPath();
     ctx.moveTo(touchstart_pos[0], touchstart_pos[1]);
     ctx.lineTo(touchmove_pos[0], touchmove_pos[1]);
     ctx.stroke();
     ctx.strokeStyle = "black";
-    touchhistory.push({'type': 'touchmove', 'x': touchmove_pos[0], 'y': touchmove_pos[1]})
+    touchhistory.push(
+        touch_event('move', touchmove_pos[0], touchmove_pos[1], 0)
+    );
     touchstart_pos = touchmove_pos;
 }
 
-function touchend(event){
-    touchend_pos = getRelativePosition(getTouches(event));
+function touchend(event) {
+    let touchend_pos = getRelativePosition(getTouches(event));
     ctx.beginPath();
     ctx.moveTo(touchstart_pos[0], touchstart_pos[1]);
     ctx.lineTo(touchmove_pos[0], touchmove_pos[1]);
     ctx.stroke();
     ctx.strokeStyle = "black";
-    touchhistory.push({'type': 'end', 'x': touchmove_pos[0], 'y': touchmove_pos[1]})
+    touchhistory.push(
+        touch_event('end', touchend_pos[0], touchend_pos[1], 1)
+    );
 }
 
-function clearCanvas(){
+function touch_event(type, x, y, pen) {
+    return {
+        'type': type,
+        'x': x,
+        'y': y,
+        'ev': pen,
+        'ts': (new Date()).getTime()
+    }
+}
+
+function clearCanvas() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     touchhistory = []
 }
 
-function getRelativePosition([x, y]){
-    return [x-offset_x, y-offset_y];
+function download() {
+    let object = {};
+    object['test2'] = {};
+    object['test2']['wholeword_segments'] = "";
+    object['test2']['word_ascii'] = "";
+    object['test2']['word_stroke'] = touchhistory;
+
+    // save
+    data = "text/json;charset=utf-8, " + encodeURIComponent(JSON.stringify(object));
+    let a = document.createElement('a');
+    a.href = 'data:' + data;
+    a.download = 'hello_world.json'
+    a.innerHTML = 'Download'
+    a.click()
 }
 
-function getTouches(event){
+function getRelativePosition([x, y]) {
+    return [x - offset_x, y - offset_y];
+}
+
+function mouseToTouch(event) {
+    event.touches = []
+    event.touches.push({
+        "clientX": event.clientX,
+        "clientY": event.clientY
+    }
+    )
+    return event
+}
+
+function getTouches(event) {
     return [event.touches[0].clientX, event.touches[0].clientY];
 }
