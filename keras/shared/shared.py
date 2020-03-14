f36e10b330a2f00da66037522af72256b8fea2bf
Martin Villavicencio <villavicencio.martin@protonmail.com>
2020-03-14 20:02:19 +0100

    made a package out of it?

---
+++ keras/shared/shared.py
@@ -0,0 +1,46 @@
+import numpy as np
+
+def parse_json(input_json):
+    strokes = input_json['word_stroke']
+    model_input = []
+
+    # read strokes
+    for stroke in strokes:
+        elem = [stroke['x'], stroke['y'], stroke['ev']]
+        model_input.append(elem)
+    model_input = np.array(model_input)
+
+    # Normalize the strokes
+    # x_normalized = (model_input[:, 0] - np.mean(model_input[:, 0])) / np.linalg.norm(model_input[:, 0])
+    # y_normalized = (model_input[:, 1] - np.mean(model_input[:, 0])) / np.linalg.norm(model_input[:, 1])
+    # model_input = np.array([x_normalized, y_normalized, model_input[:, 2]]).transpose()
+
+    # model_input = np.array([model_input[:, 0].astype('float'), model_input[:, 1].astype('float'), model_input[:, 2]]).transpose()
+
+    x_normalized = standartize_vector(calculate_diff(model_input[:, 0]))
+    y_normalized = standartize_vector(calculate_diff(model_input[:, 1]))
+    model_input = np.array([x_normalized, y_normalized, model_input[1:, 2]]).transpose()
+
+    # Add other dimension for model to predict
+    model_input = np.expand_dims(model_input, axis=0)
+    return model_input
+
+
+def calculate_diff(vector):
+    vector = np.array(vector).astype('float')
+    vector = np.diff(vector)
+    return vector
+
+
+def scale_to_zero_one(vector):
+    minimum = np.min(vector)
+    maximum = np.max(vector)
+    vector = (vector - minimum) / (maximum - minimum)
+    return vector, minimum
+
+
+def standartize_vector(vector):
+    mean = np.mean(vector)
+    std = np.std(vector)
+    vector = (vector - mean) / std
+    return vector
--- keras/simple/server.py
+++ keras/simple/server.py
@@ -4,6 +4,7 @@ from flask_cors import CORS
 from tensorflow.keras.models import load_model
 from sklearn.preprocessing import LabelEncoder
 
+from ..shared import *
 import numpy as np
 
 app = Flask(__name__)
@@ -23,53 +24,6 @@ def evaluate():
     model_output = model.predict(model_input)
     result = process_result(model_output, alphabet)
     return result
-
-
-def parse_json(input_json):
-    strokes = input_json['word_stroke']
-    model_input = []
-
-    # read strokes
-    for stroke in strokes:
-        elem = [stroke['x'], stroke['y'], stroke['ev']]
-        model_input.append(elem)
-    model_input = np.array(model_input)
-
-    # Normalize the strokes
-    # x_normalized = (model_input[:, 0] - np.mean(model_input[:, 0])) / np.linalg.norm(model_input[:, 0])
-    # y_normalized = (model_input[:, 1] - np.mean(model_input[:, 0])) / np.linalg.norm(model_input[:, 1])
-    # model_input = np.array([x_normalized, y_normalized, model_input[:, 2]]).transpose()
-
-    # model_input = np.array([model_input[:, 0].astype('float'), model_input[:, 1].astype('float'), model_input[:, 2]]).transpose()
-
-    x_normalized = standartize_vector(calculate_diff(model_input[:, 0]))
-    y_normalized = standartize_vector(calculate_diff(model_input[:, 1]))
-    model_input = np.array([x_normalized, y_normalized, model_input[1:, 2]]).transpose()
-
-    # Add other dimension for model to predict
-    model_input = np.expand_dims(model_input, axis=0)
-    return model_input
-
-
-def calculate_diff(vector):
-    vector = np.array(vector).astype('float')
-    vector = np.diff(vector)
-    return vector
-
-
-def scale_to_zero_one(vector):
-    minimum = np.min(vector)
-    maximum = np.max(vector)
-    vector = (vector - minimum) / (maximum - minimum)
-    return vector, minimum
-
-
-def standartize_vector(vector):
-    mean = np.mean(vector)
-    std = np.std(vector)
-    vector = (vector - mean) / std
-    return vector
-
 
 def process_result(result, alphabet):
     bow_positions = np.where(result[2] > 0.8)[1]
