4890f97b3dc5fd0926740652168880ed29ff7960
Martin Villavicencio <villavicencio.martin@protonmail.com>
2020-10-16 00:28:49 +0200

    changed to eow labels

--- deepreading/baseline/server.py
+++ deepreading/baseline/server.py
@@ -15,7 +15,7 @@ alphabet = list(
     "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.,-()/"
 )  # %:;&# '\x00' character, i.e., ord(0) to label concatenations.
 alphabet.insert(0, chr(0))
-model = load_model('./deepreading/baseline/model/best_model_512.hdf5')
+model = load_model('./deepreading/baseline/model/best_model.hdf5')
 
 
 @app.route('/', methods=['POST'])
@@ -31,8 +31,8 @@ def evaluate():
 
 
 def process_result(result, alphabet):
-    bow_positions = np.where(result[2] > 0.9)[1]
-    eoc_positions = np.where(result[1] > 0.9)[1]
+    eow_positions = np.where(result[2] > 0.8)[1]
+    eoc_positions = np.where(result[1] > 0.8)[1]
 
     char_prediction = result[0][0]
     argmax_char = np.argmax(char_prediction, 1)
@@ -45,17 +45,40 @@ def process_result(result, alphabet):
 
     chars = [(c,) for c in chars]
     chars = [chars[i] + char_probs[i] for i in range(len(chars))]
-    chars = [chars[p] + ('bow',) if p in bow_positions else chars[p] for p in range(len(chars))]
+    chars = [chars[p] + ('eow',) if p in eow_positions else chars[p] for p in range(len(chars))]
     chars = [chars[p] + ('eoc',) if p in eoc_positions else chars[p] for p in range(len(chars))]
 
-    if 'bow' not in chars[0]:
-        chars[0] = chars[0] + ('bow',)
     if 'eoc' not in chars[-1]:
         chars[-1] = chars[-1] + ('eoc',)
+
+    if 'eow' not in chars[-1]:
+        chars[-1] = chars[-1] + ('eow',)
 
     chars_collapsed = []
     history = []
     for idx, char in enumerate(chars):
+        if 'eoc' not in char and 'eow' not in char:
+            history.append(char)
+
+        if 'eoc' in char or 'eow' in char:
+            if history:
+                # Find most probable character based on their probability
+                probability_dict = {}
+                for h in history:
+                    if h[0] in probability_dict:
+                        probability_dict[h[0]] += h[1]
+                    else:
+                        probability_dict[h[0]] = h[1]
+
+                #most_common = max(set(history), key=history.count)
+                most_common = max(probability_dict, key=probability_dict.get)
+                chars_collapsed.append(most_common)
+            history = []
+        
+        if 'eow' in char:
+            chars_collapsed.append(" ")
+
+        '''
         if 'eoc' in char:
             if history:
                 # Find most probable character based on their probability
@@ -70,10 +93,11 @@ def process_result(result, alphabet):
                 most_common = max(probability_dict, key=probability_dict.get)
                 chars_collapsed.append(most_common)
             history = []
-        elif 'bow' in char and idx != 0:
+        elif 'eow' in char and idx != 0:
             chars_collapsed.append(" ")
         else:
             history.append(char)
+        '''
     return "".join(chars_collapsed)
 
 
