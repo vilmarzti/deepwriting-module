e188fed1ac18736cf4581b7d135cc4724b3649b4
Villavicencio Martin <villavicencio.martin@protonmail.com>
2020-10-15 13:01:32 +0200

    fixed in install script

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 sudo apt update
-sudo apt install build-essentials python3-dev libfreetype6-dev curl -y
+sudo apt install build-essential python3-dev libfreetype6-dev curl -y
 curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 -
 poetry shell
 pip install cython
