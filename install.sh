536bfe351f5630d64f1d7411ffb66556606eefe8
Villavicencio Martin <villavicencio.martin@protonmail.com>
2020-10-15 13:17:12 +0200

    updated install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 sudo apt update
 sudo apt install build-essential python3-dev libfreetype6-dev curl -y
 curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 -
+source $HOME/.poetry/env
 poetry shell
 pip install cython
 poetry install
