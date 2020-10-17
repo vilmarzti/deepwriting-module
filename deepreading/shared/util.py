f607c8a66f6b1f2e3b589658b53f2040b557f7f6
Martin Villavicencio <villavicencio.martin@protonmail.com>
2020-10-17 14:29:00 +0200

    new server.py might put it in shared

--- deepreading/shared/util.py
+++ deepreading/shared/util.py
@@ -18,9 +18,22 @@ def parse_json(input_json):
 
     # model_input = np.array([model_input[:, 0].astype('float'), model_input[:, 1].astype('float'), model_input[:, 2]]).transpose()
 
-    x_normalized = standartize_vector(calculate_diff(model_input[:, 0]))
-    y_normalized = standartize_vector(calculate_diff(model_input[:, 1]))
-    model_input = np.array([x_normalized, y_normalized, model_input[1:, 2]]).transpose()
+    x_transformed = model_input[:, 0]
+    y_transformed = model_input[:, 1]
+    
+    # x_transformed = scale_to_zero_one(model_input[:, 0])
+    # y_transformed = scale_to_zero_one(model_input[:, 1])
+
+    # x_transformed = tranlsate_to_zero(x_transformed)
+    # y_transformed = tranlsate_to_zero(y_transformed)
+
+    x_transformed = calculate_diff(x_transformed)
+    y_transformed = calculate_diff(y_transformed)
+
+    x_transformed = standartize_vector(x_transformed)
+    y_transformed = standartize_vector(y_transformed)
+
+    model_input = np.array([x_transformed, y_transformed, model_input[1:, 2]]).transpose()
 
     # Add other dimension for model to predict
     model_input = np.expand_dims(model_input, axis=0)
@@ -33,11 +46,21 @@ def calculate_diff(vector):
     return vector
 
 
+def tranlsate_to_zero(vector):
+    origin = vector[0]
+    return np.array([x - origin for x in vector])
+
+
 def scale_to_zero_one(vector):
     minimum = np.min(vector)
     maximum = np.max(vector)
     vector = (vector - minimum) / (maximum - minimum)
-    return vector, minimum
+    return vector 
+
+
+def normalize_vector(vector):
+    mean = np.mean(vector)
+    return vector - mean
 
 
 def standartize_vector(vector):
