81d1890a6033aa64948f7f4c5c167e7bcd6dda59
Martin Villavicencio <villavicencio.martin@protonmail.com>
2020-10-17 17:23:27 +0200

    updated preprocessing

--- deepreading/baseline-stacked/server.py
+++ deepreading/baseline-stacked/server.py
@@ -3,10 +3,8 @@ from flask_cors import CORS
 from waitress import serve
 
 from tensorflow.keras.models import load_model
-from sklearn.preprocessing import LabelEncoder
 
 from ..shared import util, variables
-import numpy as np
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -24,81 +22,10 @@ def evaluate():
     if len(input_json['word_stroke']) != 0:
         model_input = util.parse_json(input_json)
         model_output = model.predict(model_input)
-        result = process_result(model_output, alphabet)
+        result = util.process_result(model_output, alphabet)
         return {'result': result}
     else:
         return {'result': ''}
-
-
-def process_result(result, alphabet):
-    eow_positions = np.where(result[2] > 0.8)[1]
-    eoc_positions = np.where(result[1] > 0.8)[1]
-
-    char_prediction = result[0][0]
-    argmax_char = np.argmax(char_prediction, 1)
-
-    char_label_encoder = LabelEncoder()
-    char_label_encoder.fit(alphabet)
-
-    chars = char_label_encoder.inverse_transform(argmax_char)
-    char_probs = [(char_prediction[index, value],) for index, value in enumerate(argmax_char)]
-
-    chars = [(c,) for c in chars]
-    chars = [chars[i] + char_probs[i] for i in range(len(chars))]
-    chars = [chars[p] + ('eow',) if p in eow_positions else chars[p] for p in range(len(chars))]
-    chars = [chars[p] + ('eoc',) if p in eoc_positions else chars[p] for p in range(len(chars))]
-
-    if 'eoc' not in chars[-1]:
-        chars[-1] = chars[-1] + ('eoc',)
-
-    if 'eow' not in chars[-1]:
-        chars[-1] = chars[-1] + ('eow',)
-
-    chars_collapsed = []
-    history = []
-    for idx, char in enumerate(chars):
-        if 'eoc' not in char and 'eow' not in char:
-            history.append(char)
-
-        if 'eoc' in char or 'eow' in char:
-            if history:
-                # Find most probable character based on their probability
-                probability_dict = {}
-                for h in history:
-                    if h[0] in probability_dict:
-                        probability_dict[h[0]] += h[1]
-                    else:
-                        probability_dict[h[0]] = h[1]
-
-                #most_common = max(set(history), key=history.count)
-                most_common = max(probability_dict, key=probability_dict.get)
-                chars_collapsed.append(most_common)
-            history = []
-        
-        if 'eow' in char:
-            chars_collapsed.append(" ")
-
-        '''
-        if 'eoc' in char:
-            if history:
-                # Find most probable character based on their probability
-                probability_dict = {}
-                for h in history:
-                    if h[0] in probability_dict:
-                        probability_dict[h[0]] += h[1]
-                    else:
-                        probability_dict[h[0]] = h[1]
-
-                #most_common = max(set(history), key=history.count)
-                most_common = max(probability_dict, key=probability_dict.get)
-                chars_collapsed.append(most_common)
-            history = []
-        elif 'eow' in char and idx != 0:
-            chars_collapsed.append(" ")
-        else:
-            history.append(char)
-        '''
-    return "".join(chars_collapsed)
 
 
 def main():
--- deepreading/shared/util.py
+++ deepreading/shared/util.py
@@ -1,5 +1,6 @@
 import numpy as np
-
+from sklearn.preprocessing import LabelEncoder
+import numpy as np
 
 def parse_json(input_json):
     strokes = input_json['word_stroke']
@@ -9,53 +10,34 @@ def parse_json(input_json):
     for stroke in strokes:
         elem = [stroke['x'], stroke['y'], stroke['ev']]
         model_input.append(elem)
+
     model_input = np.array(model_input)
+    #model_input = scale_to_zero_one(model_input)
+    #model_input = translate_to_origin(model_input)
+    model_input = calculate_diff(model_input)
+    model_input = standartize_vector(model_input)
 
-    # Normalize the strokes
-    # x_normalized = (model_input[:, 0] - np.mean(model_input[:, 0])) / np.linalg.norm(model_input[:, 0])
-    # y_normalized = (model_input[:, 1] - np.mean(model_input[:, 0])) / np.linalg.norm(model_input[:, 1])
-    # model_input = np.array([x_normalized, y_normalized, model_input[:, 2]]).transpose()
-
-    # model_input = np.array([model_input[:, 0].astype('float'), model_input[:, 1].astype('float'), model_input[:, 2]]).transpose()
-
-    x_transformed = model_input[:, 0]
-    y_transformed = model_input[:, 1]
-    
-    # x_transformed = scale_to_zero_one(model_input[:, 0])
-    # y_transformed = scale_to_zero_one(model_input[:, 1])
-
-    # x_transformed = tranlsate_to_zero(x_transformed)
-    # y_transformed = tranlsate_to_zero(y_transformed)
-
-    x_transformed = calculate_diff(x_transformed)
-    y_transformed = calculate_diff(y_transformed)
-
-    x_transformed = standartize_vector(x_transformed)
-    y_transformed = standartize_vector(y_transformed)
-
-    model_input = np.array([x_transformed, y_transformed, model_input[1:, 2]]).transpose()
-
-    # Add other dimension for model to predict
+    # Add new dimension for batch_size
     model_input = np.expand_dims(model_input, axis=0)
     return model_input
 
 
-def calculate_diff(vector):
-    vector = np.array(vector).astype('float')
-    vector = np.diff(vector)
-    return vector
+def calculate_diff(model_input):
+    source = np.vstack((model_input[0], model_input))
+    model_input = np.diff(source, axis=0)
+    return model_input
 
 
-def tranlsate_to_zero(vector):
-    origin = vector[0]
-    return np.array([x - origin for x in vector])
+def translate_to_origin(model_input):
+    origin = np.append(model_input[0, [0, 1]], [0])
+    return model_input - origin
 
 
-def scale_to_zero_one(vector):
-    minimum = np.min(vector)
-    maximum = np.max(vector)
-    vector = (vector - minimum) / (maximum - minimum)
-    return vector 
+def scale_to_zero_one(model_input):
+    minimum = np.min(model_input, axis=0)
+    maximum = np.max(model_input, axis=0)
+    model_input = (model_input - minimum) / (maximum - minimum)
+    return model_input
 
 
 def normalize_vector(vector):
@@ -63,8 +45,62 @@ def normalize_vector(vector):
     return vector - mean
 
 
-def standartize_vector(vector):
-    mean = np.mean(vector)
-    std = np.std(vector)
-    vector = (vector - mean) / std
-    return vector
+def standartize_vector(model_input):
+    mean = np.mean(model_input, axis=0)
+    std = np.std(model_input, axis=0)
+    mean[2] = 0.0
+    std[2] = 1.0
+    model_input = (model_input - mean) / std
+    return model_input 
+
+
+def process_result(result, alphabet):
+    eow_positions = np.where(result[2] > 0.8)[1]
+    eoc_positions = np.where(result[1] > 0.8)[1]
+
+    char_prediction = result[0][0]
+    argmax_char = np.argmax(char_prediction, 1)
+
+    char_label_encoder = LabelEncoder()
+    char_label_encoder.fit(alphabet)
+
+    chars = char_label_encoder.inverse_transform(argmax_char)
+    char_probs = [(char_prediction[index, value],) for index, value in enumerate(argmax_char)]
+
+    chars = [(c,) for c in chars]
+    chars = [chars[i] + char_probs[i] for i in range(len(chars))]
+    chars = [chars[p] + ('eow',) if p in eow_positions else chars[p] for p in range(len(chars))]
+    chars = [chars[p] + ('eoc',) if p in eoc_positions else chars[p] for p in range(len(chars))]
+
+    if 'eoc' not in chars[-1]:
+        chars[-1] = chars[-1] + ('eoc',)
+
+    if 'eow' not in chars[-1]:
+        chars[-1] = chars[-1] + ('eow',)
+
+    chars_collapsed = []
+    history = []
+    for idx, char in enumerate(chars):
+        if 'eoc' not in char and 'eow' not in char:
+            history.append(char)
+
+        if 'eoc' in char or 'eow' in char:
+            if history:
+                # Find most probable character based on their probability
+                probability_dict = {}
+                for h in history:
+                    if h[0] in probability_dict:
+                        probability_dict[h[0]] += h[1]
+                    else:
+                        probability_dict[h[0]] = h[1]
+
+                #most_common = max(set(history), key=history.count)
+                most_common = max(probability_dict, key=probability_dict.get)
+                chars_collapsed.append(most_common)
+            history = []
+        
+        if 'eow' in char:
+            chars_collapsed.append(" ")
+
+    return "".join(chars_collapsed)
+
